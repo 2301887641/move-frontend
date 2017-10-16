6398b8dd98772b708cb7891e40b7b5f82c8051b3
2301887641 <2301887641@qq.com>
2017-10-16 15:20:26 +0800

    树形权限完善

--- src/components/authgroup/add.vue
+++ src/components/authgroup/add.vue
@@ -27,7 +27,9 @@
           node-key="id"
           ref="tree"
           highlight-current
-          :props="defaultProps">
+          :props="defaultProps"
+          @check-change="checkTree"
+        >
         </el-tree>
       <div slot="footer">
         <Button type="ghost" style="margin-left: 8px" @click="remove">取消</Button>
@@ -47,7 +49,7 @@
             modal_loading: false,
             form: {
               name: '',
-              permissions: ''
+              permissions_id: {}
             },
             ruleCustom: {
               name: [
@@ -70,10 +72,33 @@
         remove() {
           this.authgroupadd = false
         },
+        checkTree(data, node, text) {
+          // 选中子节点的情况分为两类:
+          // 1.node等于true的话说明当前被选中了 分为：如果是选择子类的话,父类不会被选中,所以需要在else中判断
+          if (node === true) {
+            this.form.permissions_id[data.id] = data.id
+          } else {
+            // 如果是父节点的话也需要加入到数组中
+             if (data.parent_id === 0) {
+               this.form.permissions_id[data.id] = data.id
+             }
+          }
+        },
         // 保存操作
         addOk() {
-            console.log(this.$refs.tree.getCheckedKeys())
-         }
+          console.log(Object.keys(this.form.permissions_id))
+          return
+//          this.$refs.formCustom.validate((valid) => {
+//            if (valid) {
+//              this.form.permissions_id = this.$refs.tree.getCheckedKeys()
+//              if (this.form.permissions_id.length === 0) {
+//                this.$message.error('请选择权限!!')
+//                return
+//              }
+//              console.log(this.form.permissions_id)
+//                }
+//              })
+            }
         }
     }
 </script>
