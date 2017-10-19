1c5ae20071c58fe41a44396536bc6b24ff5634c8
2301887641 <2301887641@qq.com>
2017-10-19 15:20:26 +0800

    完善角色

--- src/components/authgroup/add.vue
+++ src/components/authgroup/add.vue
@@ -20,110 +20,115 @@
         show-icon>
       </el-alert>
       <br/>
-        <el-tree
-          :data="permissions"
-          show-checkbox
-          accordion
-          node-key="id"
-          ref="tree"
-          highlight-current
-          :props="defaultProps"
-          @node-expand="expand"
-        >
-        </el-tree>
+      <el-tree
+        :data="permissions"
+        show-checkbox
+        accordion
+        node-key="id"
+        ref="tree"
+        highlight-current
+        :props="defaultProps"
+        @node-expand="expand"
+      >
+      </el-tree>
       <div slot="footer">
         <Button type="ghost" style="margin-left: 8px" @click="remove">取消</Button>
-        <Button type="primary"  :loading="modal_loading" @click="addOk">保存</Button>
+        <Button type="primary" :loading="modal_loading" @click="addOk">保存</Button>
       </div>
     </Modal>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-    export default{
-        data() {
-          return {
-            authgroupadd: false,
-            maskClosable: false,
-            closable: false,
-            modal_loading: false,
-            form: {
-              name: '',
-              permissions_id: {}
-            },
-            // 展开后的一级树对象 和二级树对象
-            levelOne: {},
-            levelTwo: {},
-            ruleCustom: {
-              name: [
-                { required: true, message: '请输入角色名称', trigger: 'blur' }
-              ]
-            },
-            // 指定树形结构规则 子级在children中
-            defaultProps: {
-              children: 'children',
-              label: 'name'
-            }
-          }
+  export default {
+    data() {
+      return {
+        authgroupadd: false,
+        maskClosable: false,
+        closable: false,
+        modal_loading: false,
+        form: {
+          name: '',
+          permission_id: {}
         },
-      props: {
-        permissions: {
-          default: Array
+        // 展开后的一级树对象 和二级树对象
+        levelOne: {},
+        levelTwo: {},
+        permissions_id: {},
+        ruleCustom: {
+          name: [
+            {required: true, message: '请输入角色名称', trigger: 'blur'}
+          ]
+        },
+        // 指定树形结构规则 子级在children中
+        defaultProps: {
+          children: 'children',
+          label: 'name'
+        }
+      }
+    },
+    props: {
+      permissions: {
+        default: Array
+      }
+    },
+    methods: {
+      // 取消操作
+      remove() {
+        this.authgroupadd = false
+      },
+      // 展开树
+      expand(obj, node, data) {
+        // 一级展开和二级展开
+        if (node.level === 1) {
+          this.levelOne[obj.id] = node
+        } else if (node.level === 2) {
+          this.levelTwo[obj.id] = node
         }
       },
-      methods: {
-        // 取消操作
-        remove() {
-          this.authgroupadd = false
-        },
-        // 展开树
-        expand(obj, node, data) {
-          // 一级展开和二级展开
-          if (node.level === 1) {
-             this.levelOne[obj.id] = node
-          } else if (node.level === 2) {
-             this.levelTwo[obj.id] = node
+      // 在一级或二级中查找
+      loop(id) {
+        // 如果在二级里面有的话
+        if (this.levelTwo[id]) {
+          this.permissions_id[id] = this.levelTwo[id]
+          // 如果二级有父类的话 同样需要放入进去
+          if (this.levelTwo[id].data.parent_id) {
+            this.permissions_id[this.levelTwo[id].data.parent_id] = this.levelTwo[id].data.parent_id
           }
-        },
-        loop(id) {
-          // 如果在二级里面有的话
-            if (this.levelTwo[id]) {
-              this.form.permissions_id[id] = this.levelTwo[id]
-            } else if (this.levelOne[id]) {
-              this.form.permissions_id[id] = this.levelOne[id]
-            }
-        },
-        // 保存操作
-        addOk() {
-          let checkedNodes = this.$refs.tree.getCheckedNodes()
-          if (checkedNodes.length < 1) {
-            this.$notify.error({
-              title: '错误',
-              message: '请先选择权限!!'
-            })
-          }
-          console.log(this.levelOne, this.levelTwo)
-          console.log(checkedNodes)
-          for (let v of checkedNodes) {
-            this.form.permissions_id[v.id] = v.id
-            if (v.parent_id !== 0) {
-              this.loop(v.parent_id)
-            }
-          }
-          return
-//          console.log(this.form.permissions_id)
-//          return
-//          this.$refs.formCustom.validate((valid) => {
-//            if (valid) {
-//              this.form.permissions_id = this.$refs.tree.getCheckedKeys()
-//              if (this.form.permissions_id.length === 0) {
-//                this.$message.error('请选择权限!!')
-//                return
-//              }
-//              console.log(this.form.permissions_id)
-//                }
-//              })
-            }
+          // 一级里面是否存在
+        } else if (this.levelOne[id]) {
+          this.permissions_id[id] = this.levelOne[id]
         }
+      },
+      // 保存操作
+      addOk() {
+        this.$refs.formCustom.validate((valid) => {
+          if (valid) {
+            let checkedNodes = this.$refs.tree.getCheckedNodes()
+            if (checkedNodes.length < 1) {
+              this.$notify.error({
+                title: '错误',
+                message: '请先选择权限!!'
+              })
+            }
+            // 遍历点击的节点
+            checkedNodes.forEach((v, k) => {
+              // 先放入到权限数组中
+              this.permissions_id[v.id] = v.id
+              if (v.parent_id !== 0) {
+                this.loop(v.parent_id)
+              }
+            })
+            this.form.permission_id = Object.keys(this.permissions_id)
+            let headers = this.$lockr.get('headers')
+            this.$http.post(this.$config.domain + 'authGroup', this.form, (response) => {
+              this.permissions_id = {}
+              this.$refs.formCustom.resetFields()
+              this.$parent.index()
+            }, headers)
+          }
+        })
+      }
     }
+  }
 </script>
